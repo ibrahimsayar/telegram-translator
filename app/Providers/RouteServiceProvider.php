7b0752a5a0dc0a3f03900092e764c8767495a752
ibrahim.sayar <ibrahim.sayar@naylalabs.com>
2022-05-18 18:01:43 +0300

    Rate limit disable changed.
    Wip.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,7 +35,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->configureRateLimiting();
+        //$this->configureRateLimiting();
 
         $this->routes(function () {
             Route::prefix('api')
@@ -60,8 +60,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting()
     {
-//        RateLimiter::for('api', function (Request $request) {
-//            return Limit::perMinute(1000)->by(optional($request->user())->id ?: $request->ip());
-//        });
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(1000)->by(optional($request->user())->id ?: $request->ip());
+        });
     }
 }
