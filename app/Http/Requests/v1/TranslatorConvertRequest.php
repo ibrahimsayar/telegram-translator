64ead7c221ecb90c255552cc8ceefd13f223167d
İbrahim Sayar <36553572+ibrahimsayar@users.noreply.github.com>
2023-10-17 17:37:03 +0300

    Update TranslatorConvertRequest.php

--- app/Http/Requests/v1/TranslatorConvertRequest.php
+++ app/Http/Requests/v1/TranslatorConvertRequest.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Validation\Validator;
 class TranslatorConvertRequest extends FormRequest
 {
     /**
+     *
      * Determine if the user is authorized to make this request.
      *
      * @return bool
